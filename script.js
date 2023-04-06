a1234cd7db4599b0cfe3312798362c71f9e4c8eb
Huy <trannhathuy1305@gmail.com>
2023-04-06 12:20:32 +0700

    Add loading to project

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const quoteContainer = document.getElementById(' quote-container')
+const quoteContainer = document.getElementById('quote-container')
 const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
 let apiQuotes = []
 
+// Show Loading
+function loading() {
+ loader.hidden = false
+ quoteContainer.hidden = true
+}
+
+// Hide Loading
+function complete() {
+ quoteContainer.hidden = false
+ loader.hidden = true
+}
+
 // Show New Quote
 function newQuote() {
+ loading()
  // Pick a random quote from apiQuotes array
  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
 
@@ -24,11 +38,14 @@ function newQuote() {
  } else {
   quoteText.classList.remove('long-quote')
  }
+ //  Set Quote, Hide Loader
  quoteText.textContent = quote.text
+ complete()
 }
 
 // Get Quotes From API
 // async function getQuotes() {
+// loading()
 //  const apiUrl = 'https://type.fit/api/quotes'
 //  try {
 //   const response = await fetch(apiUrl)
